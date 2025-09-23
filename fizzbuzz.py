c15d82e752190ae6f1bb1ade47f9bbc14efb2b55
pravallikamatta <pravallikayadavmsd@gmail.com>
2025-09-23 15:53:10 +0530

    1

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+total=0
+for i in range(1,11):
+    if i%3==0:
+     print("fizz") 
+    elif i%5==0:
+       print("buzz")
+    elif  i%3==0 and i%5==0:  
+       print("fizzbuzz")  
+    else:(i)    
