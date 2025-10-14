5d2cd53a25bd1e9acdba6e39a00ca7f1d631a259
pravallikamatta <pravallikayadavmsd@gmail.com>
2025-10-14 12:06:59 +0530

    adding

---
+++ silentauction.py
@@ -0,0 +1,23 @@
+import os
+def find_winner(bidder_details):
+        highest_bid=0
+        winner="" 
+        for bidder in bidder_details:
+           bidding_price=bidder_details[bidder]
+           if bidding_price>bidder_details:
+                  highest_bid=bidding_price
+                  winner=bidder
+        print(f"here is the list of all the bidders:{bidder_details}")  
+        print(f"the winner is {winner} with a bid price of {highest_bid}")        
+bidder_data={}
+endofbid=False
+while not endofbid:
+        name= input("whats your name:- ")
+        price= int(input("what much is your bid?-"))
+        morebidders=input("is there any more bidders:-").lower()
+        bidder_data[name]=price
+        if morebidders=="no":
+                endofbid=True
+                find_winner(bidder_data)
+        elif morebidders=="yes":
+                os.sysyem('cls')        
